4a8dad3a495048379c9738242f0ae5b33560c485
Emilie Kildemoes <emkil21@student.sdu.dk>
2026-04-21 22:26:01 +0200

    Fixed json

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,31 @@
-import { Component, signal } from '@angular/core';
+import { Component} from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  imports: [],
+  standalone: true, /*kræves der ngmodule eller ej */
+  selector: 'app-root', /*fortæller CSS at den skal placere dette component */
+  imports: [CommonModule], /*dependencies */
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('My angular to do list');
+  title = 'My angular to do list';
+
+  filter: "all" | "active" | "done" = "all";
+
+  /*allItems Array */
+  allItems = [
+    {description: "eat", done: true},
+    {description: "sleep", done: false},
+    {description: "have fun", done: true},
+    {description: "go for a walk", done: true},
+  ];
+
+  get items(){
+    if(this.filter === "all"){
+      return this.allItems;
+    }
+    return this.allItems.filter((item) => this.filter==="done" ? item.done : !item.done,
+    );
+  } 
 }
--- src/app/item.ts
+++ src/app/item.ts
@@ -1,3 +1,5 @@
+/*item interface laver item object model så app ved hvad item er
+Her er et item et objekt som har en description og can markeres som done*/
 export interface Item{
     description: string;
     done: boolean;
