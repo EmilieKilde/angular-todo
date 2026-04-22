37536d279e57de157ba45978a235cc86129d2775
Emilie Kildemoes <101638501+EmilieKilde@users.noreply.github.com>
2026-04-22 08:58:21 +0200

    Added edit and delete buttons + some styling

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Item } from "./item";
+import { ItemComponent } from "./item/item.component";
 
 @Component({
   standalone: true, /*kræves der ngmodule eller ej */
   selector: 'app-root', /*fortæller CSS at den skal placere dette component */
-  imports: [CommonModule], /*dependencies */
+  imports: [CommonModule, ItemComponent], /*dependencies */
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -31,7 +33,6 @@ export class App {
     });
   }
 
-
   get items(){
     if(this.filter === "all"){
       return this.allItems;
@@ -39,4 +40,8 @@ export class App {
     return this.allItems.filter((item) => this.filter==="done" ? item.done : !item.done,
     );
   } 
+   remove(item: Item) {
+    this.allItems.splice(this.allItems.indexOf(item), 1);
+  }
 }
+ 
